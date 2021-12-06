c9f2b1d4a33d672abc13425a23d39b6843135069
Vadim Galaktionov <v.galaktionov@enpicom.com>
2021-12-06 18:46:15 +0100

    ba1b tweaks

--- exercises/ch1/ba1b.ts
+++ exercises/ch1/ba1b.ts
@@ -1,24 +1,19 @@
 import { countPattern } from '../../lib';
 
 function frequentWords(text: string, k: number) {
-    // const frequencies = new DefaultDictionary<string, number>(() => 0);
     const frequencies = new Map<string, number>();
     const lastIndex = text.length - k;
     for (let i = 0; i <= lastIndex; i++) {
         const kmer = text.slice(i, i + k);
-        frequencies.set(kmer, countPattern(text, kmer));
+        if (!frequencies.has(kmer)) frequencies.set(kmer, countPattern(text.slice(i), kmer));
     }
+    const sorted = Array.from(frequencies.entries()).sort(([_, a], [__, b]) => b - a);
+    const highestFreq = sorted[0][1];
+    const mostFrequentWords: string[] = [];
 
-    let highestFreq = 0;
-    let mostFrequentWords: string[] = [];
-
-    for (const [kmer, freq] of Array.from(frequencies.entries()).sort(([_, a], [__, b]) => a - b)) {
-        if (freq === highestFreq) {
-            mostFrequentWords.push(kmer);
-        } else if (freq > highestFreq) {
-            highestFreq = freq;
-            mostFrequentWords = [kmer];
-        }
+    for (const [kmer, freq] of sorted) {
+        if (freq !== highestFreq) break;
+        mostFrequentWords.push(kmer);
     }
 
     return mostFrequentWords;
